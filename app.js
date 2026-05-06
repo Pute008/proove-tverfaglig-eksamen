7a867e68ca6df307f50a4fe444814948eb999ea5
Pute008 <felixwestby@outlook.com>
2026-05-06 21:16:47 +0200

    startup

---
+++ app.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const session = require("express-session");
+const app = express();
+
+const Database = require("better-sqlite3");
+const db = new Database("treningDatabase.db");
+
+const cors = require("cors");
+app.use(cors());
+
+const bcrypt = require("bcrypt");
+
+app.use(express.static("public"));
+
+app.use(express.json());
+
+const port = 3000
+
+app.use(
+    session({
+        secret: "hemmeligNøkkel",
+        resave: false,
+        saveUninitialized: false,
+        cookie: { secure: false }
+    })
+);
+
+function kreverInnlogging(req, res, next) {
+    if(!req.session.users) {
+        return res.redirect('/index.html');
+    }
+    next();
+}
